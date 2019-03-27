a9e9f73d2a506dcda34d12b84f3eeebd49bf2ffe
Chance Hudson <jchancehud@gmail.com>
2019-03-26 22:32:06 -0500

    Add CreateEvent path

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
+import CreateEvent from './CreateEvent';
+import Colors from './Colors';
 
 Object.assign(document.body.style, {
   'margin': 'auto',
   'font-family': 'Helvetica',
+  'background-color': Colors.white,
 });
 
 ReactDOM.render(
   <Router>
     <Route path="/" component={Home} exact />
+    <Route path="/event/create" component={CreateEvent} />
   </Router>,
   document.getElementById('app')
 );
