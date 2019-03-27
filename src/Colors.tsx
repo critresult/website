2ec522687c98f48c87f4245c23c78f608b3279f0
Chance Hudson <jchancehud@gmail.com>
2019-03-26 22:30:47 -0500

    Add color pallette

---
+++ src/Colors.tsx
@@ -0,0 +1,9 @@
+// https://coolors.co/31393c-2176ff-ffd416-f4edee-ef626c
+
+export default {
+  white: '#F4EDEE',
+  pink: '#EF626C',
+  yellow: '#FFD416',
+  blue: '#2176FF',
+  black: '#31393C',
+}
