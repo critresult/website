6fc534aeac2aa9c437848bae0d71419585f70cdc
Chance Hudson <jchancehud@gmail.com>
2019-03-29 00:25:40 -0500

    Switch to deployed api

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import EventStore from './stores/event'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://api.critresult.com'
-axios.defaults.baseURL = 'http://localhost:4000'
+// axios.defaults.baseURL = 'http://localhost:4000'
 axios.defaults.headers['content-type'] = 'application/json'
 
 Object.assign(document.body.style, {
