0b4aad04d419eb6bd22d06311bc697e67c0d5614
Chance Hudson <jchancehud@gmail.com>
2019-03-29 02:30:02 -0500

    Working race creation and listing

--- src/stores/race.ts
+++ src/stores/race.ts
@@ -1,9 +1,11 @@
 import { observable } from 'mobx'
 import axios from 'axios'
+import PromoterStore from './promoter'
 
 export interface Race {
   _id: string
   name: string
+  scheduledStart: string
 }
 
 export default class RaceStore {
@@ -21,6 +23,20 @@ export default class RaceStore {
       this.racesById[data._id] = data
     } catch (err) {
       console.log('Error loading races by event id', err)
+      throw err
+    }
+  }
+
+  async create(raceData: any) {
+    try {
+      console.log(raceData)
+      const { data } = await axios.post('/races', {
+        ...raceData,
+        token: PromoterStore.activeToken(),
+      })
+      this.racesById[data._id] = data
+    } catch (err) {
+      console.log('Error creating race', err)
       throw err
     }
   }
