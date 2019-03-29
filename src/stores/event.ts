03437c9c8ac29366a66ac39882d69ea4972f6e7f
Chance Hudson <jchancehud@gmail.com>
2019-03-28 21:53:38 -0500

    Add base event store logic

--- src/stores/event.ts
+++ src/stores/event.ts
@@ -1,5 +1,28 @@
-import { observable } from 'mobx';
+import { observable } from 'mobx'
+import axios from 'axios'
+import PromoterStore from './promoter'
+import uniqBy from 'lodash.uniqby'
+
+interface Event {
+  _id: string
+  name: string
+}
 
 export default class EventStore {
-  
+  @observable upcomingEvents: Event[] = []
+
+  async loadUpcoming() {
+    try {
+      const { data } = await axios.get('/event/upcoming', {
+        params: {
+          token: PromoterStore.activeToken(),
+        },
+      })
+      const redundantEvents = [...this.upcomingEvents, ...data]
+      this.upcomingEvents = uniqBy(redundantEvents, '_id')
+    } catch (err) {
+      console.log('Error loading upcoming events', err)
+      throw err
+    }
+  }
 }
