80bb7428660bc326d2ecc968296d832e530f8719
Chance Hudson <jchancehud@gmail.com>
2019-04-13 15:57:41 -0500

    Accept style prop

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 import { VFlex } from './Shared'
 
 export default class LoadingIndicator extends React.Component<{
+  style?: any
   height?: number
 }> {
   render() {
     return (
-      <VFlex style={{ margin: 8, fontSize: 25, justifyContent: 'center' }}>
+      <VFlex
+        style={{
+          margin: 8,
+          fontSize: 25,
+          justifyContent: 'center',
+          ...(this.props.style || {}),
+        }}
+      >
         <img
           src={require('../../static/bikeLoading.svg')}
           height={this.props.height || 75}
